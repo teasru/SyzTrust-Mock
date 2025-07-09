849cdf73589bb78223be452eab4486f33e4fa588
Teasroo <190295607+teasru@users.noreply.github.com>
2025-07-09 13:35:44 +0530

    Delete Mock2 directory

--- Mock2/mock_runtime/mock_executor.py
+++
@@ -1,6 +0,0 @@
-import random
-from .mock_feedback import get_fake_feedback
-
-def execute_testcase(payload):
-    print(f"Executing syscall sequence: {payload}")
-    return get_fake_feedback(payload)
